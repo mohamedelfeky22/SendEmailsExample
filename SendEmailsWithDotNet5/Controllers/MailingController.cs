8ebfc8c0aeb5dc2ee1761e448b8f8e3d5852d4da
mohamed elfeky <mohamedelfeky700@outlook.com>
2022-04-20 13:38:56 +0200

    Add project files.

---
+++ SendEmailsWithDotNet5/Controllers/MailingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendEmailsWithDotNet5.Services;
+using SendEmailsWithDotNet5.Dtos;
+using System.Threading.Tasks;
+
+namespace SendEmailsWithDotNet5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailingController : ControllerBase
+    {
+        private readonly IMailingService _mailingService;
+
+        public MailingController(IMailingService mailingService)
+        {
+            _mailingService = mailingService;
+        }
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMail([FromForm] MailRequestDto dto)
+        {
+            await _mailingService.SendEmailAsync(dto.ToEmail, dto.Subject, dto.Body, dto.Attachments);
+            return Ok();
+        }
+
+        [HttpPost("send-welcoome-email")]
+        public async Task<IActionResult> SendWelcome(WelcomeEmailtDto dto)
+        {
+            await _mailingService.SendWelcomeEmailAsync(dto);
+            return Ok();
+        }
+
+    }
+}
---
+++ SendEmailsWithDotNet5/Services/IMailingService.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using SendEmailsWithDotNet5.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+ 
+namespace SendEmailsWithDotNet5.Services
+{
+    public interface IMailingService
+    {
+        Task SendEmailAsync(string mailTo, string subject, string body, IList<IFormFile> attachments = null);
+
+        Task SendWelcomeEmailAsync(WelcomeEmailtDto dto);
+    }
+}
